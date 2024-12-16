bfe70daa16641a9450d916e803dd970e66311736
D`Souza, Cedric (Cedric) <dsouzac@avaya.com>
2024-12-16 21:02:00 +0530

    Updated

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,6 +1,6 @@
 # src/services/__init__.py
 
-from src.services.database import get_data, update_data
+from services.database import get_data, update_data
 
 def fetch_all_data():
     query = {}  # Define your query here
